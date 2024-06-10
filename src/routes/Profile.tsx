5c7af9aa7b977588dff0bb25463bea65f0344633
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2024-06-09 21:30:05 -0300

    fix: remove some perfil fake information

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -4,6 +4,7 @@ import { AiOutlineWhatsApp } from "react-icons/ai";
 
 // images
 import profileImage from "../../public/women.jpg";
+import { FaUser } from 'react-icons/fa';
 
 const Profile = () => {
   return (
@@ -18,24 +19,20 @@ const Profile = () => {
         <div className="flex items-center justify-around w-full">
           <div className="flex flex-col w-[50vw] h-screen gap-5">
             <div className="flex items-center gap-10 ml-[8%] mt-10">
-              <img
-                src={profileImage}
-                alt="Usuário"
-                className="w-[200px] h-[200px] rounded-full"
-              />
+            <FaUser size={120} className="text-[#1D8BC9]"/> 
               <div className="flex flex-col gap-2">
                 <h3 className="text-2xl text-[#1D8BC9] font-bold">
-                  Maria Silva
+                  Nome do usuário
                 </h3>
-                <p className="text-xl font-semibold">Engenharia De Software</p>
+                <p className="text-xl font-semibold">Curso</p>
                 <p className="text-xl font-semibold flex items-center gap-2">
                   <AiOutlineWhatsApp size={24} className="text-[#1D8BC9]" />
-                  <span className="text-black">(87) 98113-2735</span>
+                  <span className="text-black">(00) 00000-0000</span>
                 </p>
               </div>
             </div>
 
-            <div className="flex items-center justify-center gap-5">
+            <div className="flex items-center justify-center gap-5 mt-10">
               <button className="bg-[#D9D9D9] hover:bg-[#1D8BC9] hover:text-white rounded-3xl p-1 w-[120px] font-bold text-[#5B5B5B] transition-all duration-300">
                 Perdidos
               </button>
